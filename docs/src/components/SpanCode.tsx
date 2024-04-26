df275b7750728ac5ffe89db4ed8918ad177ef793
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-04-26 22:09:04 +0800

    Fix: Code colors

--- docs/src/components/SpanCode.tsx
+++ docs/src/components/SpanCode.tsx
@@ -5,15 +5,12 @@ interface SpanCodeProps {
 
 export default function SpanCode(props: SpanCodeProps) {
   const { text, color } = props;
-  return (
-    <span
-      className={
-        color == "primary" || color == "white"
-          ? `text-${color}`
-          : `text-${color}-400`
-      }
-    >
-      {text}
-    </span>
-  );
+  const fixedColors = {
+    red: "text-red-400",
+    yellow: "text-yellow-400",
+    purple: "text-purple-400",
+    white: "text-white",
+    primary: "text-primary",
+  };
+  return <span className={fixedColors[color]}>{text}</span>;
 }

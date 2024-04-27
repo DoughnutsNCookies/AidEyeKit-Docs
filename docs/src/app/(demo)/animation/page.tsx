8ff7f2ce639e2170d06e40e239c591b7c95563bc
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-04-28 03:08:26 +0800

    Feat: Added documentation coming soon

--- docs/src/app/(demo)/animation/page.tsx
+++ docs/src/app/(demo)/animation/page.tsx
@@ -10,6 +10,7 @@ export default function Animation() {
       <div className="flex flex-col gap-10 border-primary/30 px-4 py-10 sm:border-l-1 sm:px-10 sm:py-20 xl:w-[50vw] xl:min-w-[50vw]">
         <div className="flex flex-col gap-2">
           <h1 className="text-4xl font-bold sm:text-5xl">Animation</h1>
+          <p>Documentations coming soon!</p>
         </div>
         <div className="flex flex-row justify-between">
           <NextLink href="/highlighting" className="w-[45%]">

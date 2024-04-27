7c0a8093e4e842d2b880cae374cc77ef54674220
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-04-27 18:22:38 +0800

    Feat: Added Green color

--- docs/src/components/SpanCode.tsx
+++ docs/src/components/SpanCode.tsx
@@ -1,6 +1,14 @@
 interface SpanCodeProps {
   text: string;
-  color: "primary" | "red" | "purple" | "white" | "grey" | "orange" | "cyan";
+  color:
+    | "primary"
+    | "red"
+    | "purple"
+    | "white"
+    | "grey"
+    | "orange"
+    | "cyan"
+    | "green";
 }
 
 export default function SpanCode(props: SpanCodeProps) {
@@ -10,6 +18,7 @@ export default function SpanCode(props: SpanCodeProps) {
     purple: "text-purple-300",
     orange: "text-orange-400",
     cyan: "text-cyan-300",
+    green: "text-green-300",
     white: "text-white",
     primary: "text-primary",
     grey: "text-white/60",
